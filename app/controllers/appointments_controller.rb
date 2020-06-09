0c81fb76aeaf6bc7b1ab0222d5e6019530d1f071
proiacm <proiacm@gmail.com>
2020-06-09 17:08:07 -0600

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,8 @@
+class AppointmentsController < ApplicationController
+   
+    def show 
+        @appointment = Appointment.find_by_id(params[:id])
+        @doctor = @appointment.doctor
+        @patient = @appointment.patient
+    end
+end
